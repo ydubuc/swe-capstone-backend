fc1298212593c7cf3e01d8891ecea596a44d2611
Yoan <yoan.dubuc@outlook.com>
2022-02-22 19:20:33 -0500

    added helmet

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 main();
 
 async function main() {
     const app = await NestFactory.create(AppModule);
     app.enableCors();
+    app.use(helmet());
     await app.listen(process.env.PORT || 3000);
 }
