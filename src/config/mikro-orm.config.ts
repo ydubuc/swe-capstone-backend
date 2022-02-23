cc776367ee2c35c387361f9293f313ab9303afec
Yoan <yoan.dubuc@outlook.com>
2022-02-22 19:17:51 -0500

    Update mikro-orm.config.ts

--- src/config/mikro-orm.config.ts
+++ src/config/mikro-orm.config.ts
@@ -1,10 +1,10 @@
 import { Options } from '@mikro-orm/core';
 
-const mikroOrmModuleOptions = {
+const mikroOrmModuleOptions: Options = {
     entities: ['dist/**/*.entity{.ts,.js}'],
     entitiesTs: ['src/**/*.entity{.ts,.js}'],
     dbName: 'sqlite-db',
     type: 'sqlite',
-} as Options;
+};
 
 export default mikroOrmModuleOptions;
