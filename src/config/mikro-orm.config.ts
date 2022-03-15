98001d5960f9c9164e6cc7d9d322ff68f6d2833c
Yoan <yoan.dubuc@outlook.com>
2022-03-15 18:50:18 -0400

    Testing heroku deployment with postgresql database

--- src/config/mikro-orm.config.ts
+++ src/config/mikro-orm.config.ts
@@ -1,10 +1,17 @@
 import { Options } from '@mikro-orm/core';
 
+// const mikroOrmModuleOptions: Options = {
+//     entities: ['dist/**/*.entity{.ts,.js}'],
+//     entitiesTs: ['src/**/*.entity{.ts,.js}'],
+//     dbName: 'sqlite-db',
+//     type: 'sqlite',
+// };
+
 const mikroOrmModuleOptions: Options = {
     entities: ['dist/**/*.entity{.ts,.js}'],
     entitiesTs: ['src/**/*.entity{.ts,.js}'],
-    dbName: 'sqlite-db',
-    type: 'sqlite',
+    type: 'postgresql',
+    clientUrl: process.env.DATABASE_URL,
 };
 
 export default mikroOrmModuleOptions;
