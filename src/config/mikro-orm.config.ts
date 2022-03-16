c889a13795310087e5603b8919e027c14828fa8d
Yoan <yoan.dubuc@outlook.com>
2022-03-15 20:39:58 -0400

    Update mikro-orm.config.ts

--- src/config/mikro-orm.config.ts
+++ src/config/mikro-orm.config.ts
@@ -10,11 +10,15 @@ import { Options } from '@mikro-orm/core';
 const mikroOrmModuleOptions: Options = {
     entities: ['dist/**/*.entity{.ts,.js}'],
     entitiesTs: ['src/**/*.entity{.ts,.js}'],
-    type: 'postgresql',
-    clientUrl: process.env.DATABASE_URL,
-    driverOptions: {
-        connection: { ssl: { rejectUnauthorized: false } },
-    },
+    type: 'sqlite',
+    dbName: 'sqlite-db',
+
+    // type: 'postgresql',
+    // clientUrl: process.env.DATABASE_URL,
+    // driverOptions: {
+    //     connection: { ssl: { rejectUnauthorized: false } },
+    // },
+
     migrations: {
         path: 'dist/migrations',
         pathTs: 'src/migrations',
