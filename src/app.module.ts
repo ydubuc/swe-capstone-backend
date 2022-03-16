c57c9de9d93fbf538e2db719de120a5bd6ca34d9
Yoan <yoan.dubuc@outlook.com>
2022-03-15 21:29:42 -0400

    commit db migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,15 @@ import { AppService } from './app.service';
 import { TicketsModule } from './tickets/tickets.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { MikroORM } from '@mikro-orm/core';
 
 @Module({
     imports: [MikroOrmModule.forRoot(), TicketsModule, AuthModule, UsersModule],
     controllers: [AppController],
     providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+    constructor(private orm: MikroORM) {
+        orm.getMigrator().up();
+    }
+}
