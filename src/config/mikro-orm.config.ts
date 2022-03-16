68194254b3a98745907ddb18cbce8bab4ebc23f3
Yoan <yoan.dubuc@outlook.com>
2022-03-15 21:06:45 -0400

    Update mikro-orm.config.ts

--- src/config/mikro-orm.config.ts
+++ src/config/mikro-orm.config.ts
@@ -1,24 +1,17 @@
 import { Options } from '@mikro-orm/core';
 
-// const mikroOrmModuleOptions: Options = {
-//     entities: ['dist/**/*.entity{.ts,.js}'],
-//     entitiesTs: ['src/**/*.entity{.ts,.js}'],
-//     dbName: 'sqlite-db',
-//     type: 'sqlite',
-// };
-
 const mikroOrmModuleOptions: Options = {
     entities: ['dist/**/*.entity{.ts,.js}'],
     entitiesTs: ['src/**/*.entity{.ts,.js}'],
-    type: 'sqlite',
-    dbName: 'sqlite-db',
-
-    // type: 'postgresql',
-    // clientUrl: process.env.DATABASE_URL,
-    // driverOptions: {
-    //     connection: { ssl: { rejectUnauthorized: false } },
-    // },
-
+    type: 'postgresql',
+    // type: 'sqlite',
+    // dbName: 'sqlite-db',
+    clientUrl: process.env.DATABASE_URL,
+    // clientUrl:
+    //     'postgres://idddewcqhuummu:9a7e98b9419912abb2dc4125954133d6bc155a1ff5e3d03950588a9fe43625c0@ec2-52-207-74-100.compute-1.amazonaws.com:5432/der905e49rrvvl',
+    driverOptions: {
+        connection: { ssl: { rejectUnauthorized: false } },
+    },
     migrations: {
         path: 'dist/migrations',
         pathTs: 'src/migrations',
