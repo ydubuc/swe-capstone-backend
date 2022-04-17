195321e6a6f997f923963642fb94e67b6a556443
Yoan <yoan.dubuc@outlook.com>
2022-04-17 14:25:43 -0400

    reset migrations

--- src/config/mikro-orm.config.ts
+++ src/config/mikro-orm.config.ts
@@ -22,7 +22,9 @@ const mikroOrmModuleOptions: Options = {
     entities: ['dist/**/*.entity{.ts,.js}'],
     entitiesTs: ['src/**/*.entity{.ts,.js}'],
     type: 'postgresql',
-    clientUrl: process.env.DATABASE_URL,
+    clientUrl:
+        process.env.DATABASE_URL ||
+        'postgres://ijzxwyrxzsgbcn:6652b74df19f3aa581f5a7a0cfb666822ce5dc08f32581f1fdba66df1ebc9ff3@ec2-34-194-158-176.compute-1.amazonaws.com:5432/d1m37bps9eq9ut',
     driverOptions: {
         connection: { ssl: { rejectUnauthorized: false } },
     },
